25be8aae1b10dc8e36ba47fc17e56ef6eb721d09
Raghav Khullar <42183326+RaghavK16@users.noreply.github.com>
2020-10-08 19:12:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import pywhatkit as kit
 
-kit.sendwhatmsg("+919871751110", "Hello mister Mumma!!", 17, 33)
+kit.sendwhatmsg("+919871751110", "Hello there, howdy!!", 17, 33)
 # kit.sendwhatmsg("The Number", "The Message", "Hour", "Minute")
